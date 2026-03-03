acf340870388abfcee7d074b356db9197618504b
siddhpatel3110-tech <siddh-patel@live.nmit.ac.nz>
2026-03-04 11:15:11 +1300

    learning string methods

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('Hello world')
 var name = "Alice"; // Function, hoisted - avoid in modern JS
-let age = 25;       // Block-scoped, can be reassgined
+let age = 25;       
 const PI = 3.14159; // Blocke-scoped, cannot be reassigned
 
 let score = 0;      
@@ -8,3 +8,19 @@ score = 10;         // OK - reassignment allowed for let
 
 const MAX = 100;
 //MAX = 200;       // TypeError: Assignment to constant variable 
+let homeAddress = "69 Washbourn";
+let userage = 20;
+console.log(homeAddress)
+
+const msg = "Hello, Javascript1";
+
+msg.length
+msg.toUpperCase()
+msg.toLowerCase()
+msg.includes("Java")
+msg.startsWith("He")
+msg.indexOf("Java")
+msg.slice(7, 17)
+msg.replace("Hello, Hi")
+msg.split(", ")
+" hi ".trim()
